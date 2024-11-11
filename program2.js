7995aca96875519f13e80c23c3a4589d450ed6d4
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-11-11 14:17:15 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,24 +2,24 @@ const decodeTheRing = function (s, p) {
 
   // write your code here
   const msgLen = s.length, patLen = p.length;
-  const dpTable = Array.from({ length: msgLen + 1 }, () => Array(patLen + 1).fill(false));
-  dpTable[0][0] = true;
+  const dp = Array.from({ length: msgLen + 1 }, () => Array(patLen + 1).fill(false));
+  dp[0][0] = true;
   for (let col = 1; col <= patLen; col++) {
-    if (p[col - 1] === '*') dpTable[0][col] = dpTable[0][col - 1];
+    if (p[col - 1] === '*') dp[0][col] = dp[0][col - 1];
   }
   for (let row = 1; row <= msgLen; row++) {
     for (let col = 1; col <= patLen; col++) {
       if (p[col - 1] === '*') {
-        dpTable[row][col] = dpTable[row][col - 1] || dpTable[row - 1][col];
+        dp[row][col] = dp[row][col - 1] || dp[row - 1][col];
       } else if (p[col - 1] === '?') {
-        dpTable[row][col] = dpTable[row - 1][col - 1];
+        dp[row][col] = dp[row - 1][col - 1];
       } else {
-        dpTable[row][col] = dpTable[row - 1][col - 1] && s[row - 1] === p[col - 1];
+        dp[row][col] = dp[row - 1][col - 1] && s[row - 1] === p[col - 1];
       }
     }
   }
 
-  return dpTable[msgLen][patLen];
+  return dp[msgLen][patLen];
 
 
 };
