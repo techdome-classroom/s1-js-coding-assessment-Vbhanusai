274c6c97786c59acdde9e229d028e7d66ef24eee
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-11-11 14:13:36 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,12 +3,10 @@ const decodeTheRing = function (s, p) {
   // write your code here
   const msgLen = s.length, patLen = p.length;
   const dpTable = Array.from({ length: msgLen + 1 }, () => Array(patLen + 1).fill(false));
-
   dpTable[0][0] = true;
   for (let col = 1; col <= patLen; col++) {
     if (p[col - 1] === '*') dpTable[0][col] = dpTable[0][col - 1];
   }
-
   for (let row = 1; row <= msgLen; row++) {
     for (let col = 1; col <= patLen; col++) {
       if (p[col - 1] === '*') {
